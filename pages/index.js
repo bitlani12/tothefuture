0ed853006281a972c7385e9675b401d948400e77
rudraj010 <rajendrakumar25.rk11@gmail.com>
2023-12-15 22:27:14 +0530

    duplicate code removed

--- pages/index.js
+++ pages/index.js
@@ -75,38 +75,6 @@ const Home = ({ frontmatter }) => {
         </div>
       </section>
 
-
-       {/* Features */}
-       <section className="section bg-theme-light">
-        <div className="container">
-          <div className="text-center">
-            <h2>{markdownify(feature1.title)}</h2>
-          </div>
-          <div className="mt-8 grid gap-x-8 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
-            {feature.features.map((item, i) => (
-              <div
-                className="feature-card rounded-xl bg-white p-5 pb-8 text-center"
-                key={`feature-${i}`}
-              >
-                {item.icon && (
-                  <Image
-                    className="mx-auto"
-                    src={item.icon}
-                    width={30}
-                    height={30}
-                    alt=""
-                  />
-                )}
-                <div className="mt-4">
-                  {markdownify(item.name, "h3", "h5")}
-                  <p className="mt-3">{item.content}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
       {/* services */}
       {services.map((service, index) => {
         const isOdd = index % 2 > 0;
@@ -204,12 +172,3 @@ export const getStaticProps = async () => {
 };
 
 export default Home;
-
-
-
-
-
-
-
-
-
